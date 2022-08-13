bd4982c2079c78f7faccf1d017b60c463e9fcd0b
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-08-12 22:01:34 -0400

    Ensure process is closed before upgrade/uninstall to increase chances of operation success

---
+++ tools/chocolateybeforemodify.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop'
+
+$softwareName = 'ControlMyMonitor'
+$processName = $softwareName.ToLower()
+$process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+if ($process)
+{
+    Write-Warning "$softwareName is currently running, stopping it to prevent upgrade/uninstall from failing..."
+    Stop-Process -InputObject $process -ErrorAction SilentlyContinue
+
+    Start-Sleep -Seconds 3
+
+    $process = Get-Process -Name $processName -ErrorAction SilentlyContinue
+    if ($process)
+    {
+        Write-Warning "$softwareName is still running despite stop request, force stopping it..."
+        Stop-Process -InputObject $process -Force -ErrorAction SilentlyContinue
+    }
+
+    Write-Warning "If upgrading, $softwareName may need to be manually restarted upon completion"
+}
+else
+{
+    Write-Debug "No running $processName process instances were found"
+}
