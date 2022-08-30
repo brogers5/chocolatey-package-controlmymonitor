82944af8192c52e361afccb4ebe77e67d10a13a5
brogers5 <6869577+brogers5@users.noreply.github.com>
2022-08-30 02:45:26 -0400

    Add support for verification against officially published checksums

--- update.ps1
+++ update.ps1
@@ -4,15 +4,10 @@ $currentPath = (Split-Path $MyInvocation.MyCommand.Definition)
 $toolsPath = Join-Path -Path $currentPath -ChildPath 'tools'
 
 $projectUri = 'https://www.nirsoft.net/utils/control_my_monitor.html'
+$checksumsUrl = 'https://www.nirsoft.net/hash_check/?software=controlmymonitor'
+$userAgent = 'Update checker of Chocolatey Community Package ''controlmymonitor'''
 
-function global:au_BeforeUpdate ($Package)  {
-    Get-RemoteFiles -Purge -NoSuffix -Algorithm sha256
-
-    if (!(Test-Path -Path "$env:PROGRAMFILES\Mozilla Firefox\firefox.exe"))
-    {
-        choco install firefox -y
-    }
-
+function Add-ArchivedUrls {
     $seleniumModuleName = 'Selenium'
     if (!(Get-Module -ListAvailable -Name $seleniumModuleName))
     {
@@ -20,11 +15,56 @@ function global:au_BeforeUpdate ($Package)  {
     }
     Import-Module $seleniumModuleName
 
-    $startUrl = "https://web.archive.org/save/$($Latest.Url32)"
-    Write-Host "Starting Selenium at $startUrl"
-    $seleniumDriver = Start-SeFirefox -StartURL $startUrl -Headless
-    $Latest.ArchivedURL = $seleniumDriver.Url
+    if (!(Test-Path -Path "$env:PROGRAMFILES\Mozilla Firefox\firefox.exe"))
+    {
+        choco install firefox -y
+    }
+
+    $checksumsArchiveUrl = "https://web.archive.org/save/$checksumsUrl"
+    Write-Host "Starting Selenium at $checksumsArchiveUrl"
+    $seleniumDriver = Start-SeFirefox -StartURL $checksumsArchiveUrl -Headless
+    $Latest.ArchivedChecksumsURL = $seleniumDriver.Url
     $seleniumDriver.Dispose()
+
+    $downloadUrl = "https://web.archive.org/save/$($Latest.Url32)"
+    Write-Host "Starting Selenium at $downloadUrl"
+    $seleniumDriver = Start-SeFirefox $downloadUrl -Headless
+    $Latest.ArchivedDownloadURL = $seleniumDriver.Url    
+    $seleniumDriver.Dispose()
+}
+
+function Read-ExpectedChecksums {
+    $checksumsResponse = Invoke-WebRequest -Uri $checksumsUrl -UserAgent $userAgent -UseBasicParsing
+
+    $Latest.ExpectedChecksumMD5 = [Regex]::Matches($checksumsResponse.Content, '\b[a-f0-9]{32}\b').Groups[0].Value
+    $Latest.ExpectedChecksumSHA1 = [Regex]::Matches($checksumsResponse.Content, '\b[a-f0-9]{40}\b').Groups[0].Value
+    $Latest.ExpectedChecksumSHA256 = [Regex]::Matches($checksumsResponse.Content, '\b[a-f0-9]{64}\b').Groups[0].Value
+    $Latest.ExpectedChecksumSHA512 = [Regex]::Matches($checksumsResponse.Content, '\b[a-f0-9]{128}\b').Groups[0].Value
+}
+
+function Confirm-Checksum($FilePath, $Algorithm, $ExpectedHash) {
+    $hash = (Get-FileHash -Path $FilePath -Algorithm $Algorithm).Hash
+    if ($ExpectedHash -ne $hash)
+    {
+        throw "$Algorithm checksum mismatch! Expected '$ExpectedHash', actual is '$hash'"
+    }
+}
+
+function Confirm-Checksums {
+    $filePath = Join-Path -Path $toolsPath -ChildPath $Latest.FileName32
+
+    Confirm-Checksum -FilePath $filePath -Algorithm 'MD5' -ExpectedHash $Latest.ExpectedChecksumMD5
+    Confirm-Checksum -FilePath $filePath -Algorithm 'SHA1' -ExpectedHash $Latest.ExpectedChecksumSHA1
+    Confirm-Checksum -FilePath $filePath -Algorithm 'SHA256' -ExpectedHash $Latest.ExpectedChecksumSHA256
+    Confirm-Checksum -FilePath $filePath -Algorithm 'SHA512' -ExpectedHash $Latest.ExpectedChecksumSHA512
+}
+
+function global:au_BeforeUpdate ($Package) {
+    Read-ExpectedChecksums
+    Get-RemoteFiles -Purge -NoSuffix
+    Confirm-Checksums
+
+    Add-ArchivedUrls
 
     Copy-Item -Path "$toolsPath\VERIFICATION.txt.template" -Destination "$toolsPath\VERIFICATION.txt" -Force
 
@@ -43,22 +83,24 @@ function global:au_AfterUpdate ($Package)  {
 function global:au_SearchReplace {
     @{
         'build.ps1' = @{
-            '(^\s*Url32\s*=\s*)(''.*'')' = "`$1'$($Latest.ArchivedURL)'"
+            '(^\s*Url32\s*=\s*)(''.*'')' = "`$1'$($Latest.ArchivedDownloadURL)'"
         }
         "$($Latest.PackageName).nuspec" = @{
             "<packageSourceUrl>[^<]*</packageSourceUrl>" = "<packageSourceUrl>https://github.com/brogers5/chocolatey-package-$($Latest.PackageName)/tree/v$($Latest.Version)</packageSourceUrl>"
             "<copyright>[^<]*</copyright>" = "<copyright>Copyright (c) 2017-$(Get-Date -Format yyyy) Nir Sofer</copyright>"
         }
         'tools\VERIFICATION.txt' = @{
-            '%snapshotUrl%' = "$($Latest.ArchivedURL)"
-            '%checksumType%' = "$($Latest.ChecksumType32.ToUpper())"
-            '%checksumValue%' = "$($Latest.Checksum32)"
+            '%archivedDownloadUrl%' = "$($Latest.ArchivedDownloadURL)"
+            '%archivedChecksumsUrl%' = "$($Latest.ArchivedChecksumsURL)"
+            '%md5Hash%' = "$($Latest.ExpectedChecksumMD5)"
+            '%sha1Hash%' = "$($Latest.ExpectedChecksumSHA1)"
+            '%sha256Hash%' = "$($Latest.ExpectedChecksumSHA256)"
+            '%sha512Hash%' = "$($Latest.ExpectedChecksumSHA512)"
         }
     }
 }
 
 function global:au_GetLatest {
-    $userAgent = 'Update checker of Chocolatey Community Package ''controlmymonitor'''
     $script:response = Invoke-WebRequest -Uri $projectUri -UserAgent $userAgent -UseBasicParsing
 
     $version = [Regex]::Matches($response.Content, '<td>ControlMyMonitor v(\d+(\.\d+){1,3})').Groups[1].Value
